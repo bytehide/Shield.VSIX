c38cdf5b414bdf998e5dfc1da1246e9274b0990b
Víctor Domínguez <hello@itandfeel.com>
2024-02-07 11:46:06 -0400

    ui improved

--- Common/Helpers/Utils.cs
+++ Common/Helpers/Utils.cs
@@ -1,7 +1,36 @@
-﻿namespace ShieldVSExtension.Common.Helpers;
+﻿using System.Diagnostics;
+using System.Reflection;
+using System;
+
+namespace ShieldVSExtension.Common.Helpers;
+
+public struct Version
+{
+    public string Major;
+    public string Minor;
+    public string Build;
+    public string Get() => $"{Major}.{Minor}.{Build}";
+}
 
 internal static class Utils
 {
+    public static Version GetVersionNumber()
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+        var versionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+        var versions = versionInfo.FileVersion.Split('.');
+        Version version = default;
+        if (versions.Length < 3) return version;
+
+        version.Major = versions[0];
+        version.Minor = versions[1];
+        version.Build = versions[2];
+
+        return version;
+    }
+
     public static string Truncate(this string value, int maxLength, string tail = "...") =>
         value.Length <= maxLength ? value : $"{value.Substring(0, maxLength)}{tail}";
+
+    public static void GoToWebsite(string path) => Process.Start(path);
 }
---
+++ UI/UserControls/AboutControl.xaml.cs
@@ -0,0 +1,32 @@
+﻿using ShieldVSExtension.Common.Helpers;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ShieldVSExtension.UI.UserControls;
+
+/// <summary>
+/// Interaction logic for AboutControl.xaml
+/// </summary>
+public partial class AboutControl
+{
+    public AboutControl()
+    {
+        InitializeComponent();
+
+        var version = Utils.GetVersionNumber();
+        VersionBox.Text = $"{version.Get()}";
+        // CopyBox.Text = $"© 2012-{DateTime.Now.Year}, ByteHide Solutions S.L. All rights reserved";
+    }
+
+    private void ButtonSiteOnClick(object sender, RoutedEventArgs e)
+    {
+        if (sender is not Button) return;
+        Utils.GoToWebsite("https://www.bytehide.com");
+    }
+
+    private void ButtonTwitterOnClick(object sender, RoutedEventArgs e)
+    {
+        if (sender is not Button) return;
+        Utils.GoToWebsite("https://twitter.com/byte_hide");
+    }
+}
